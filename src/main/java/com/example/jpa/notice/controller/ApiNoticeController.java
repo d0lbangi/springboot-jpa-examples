e847dcfc9a03b5251524a25d2bf1c3115addac23
Jeongho Park <d0lbangi13@naver.com>
2023-10-13 00:24:32 +0900

    Create an API to handle requests for a list of notice boards

--- src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
+++ src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
@@ -6,14 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ApiNoticeController {
+    /*
+    @GetMapping("/api/notice")
+    public String noticeString() {
+        return "공지사항입니다.";
+    }
+    */
 
-//    @GetMapping("/api/notice")
-//    public String noticeString() {
-//        return "공지사항입니다.";
-//    }
+    /*
     @GetMapping("/api/notice")
     public NoticeModel notice() {
 
@@ -26,5 +31,37 @@ public class ApiNoticeController {
         notice.setRegDate(regDate);
 
         return notice;
+    }
+     */
+
+
+/*    @GetMapping("/api/notice")
+    public List<NoticeModel> notice() {
+
+        List<NoticeModel> noticeList = new ArrayList<>();
+
+        noticeList.add(NoticeModel.builder()
+                .id(1)
+                .title("공지사항입니다.")
+                .contents("공지사항내용입니다.")
+                .regDate(LocalDateTime.of(2021, 1, 30, 0, 0))
+                .build());
+
+        noticeList.add(NoticeModel.builder()
+                .id(2)
+                .title("두번째 공지사항입니다.")
+                .contents("두번째 공지사항내용입니다.")
+                .regDate(LocalDateTime.of(2021, 1, 31, 0, 0))
+                .build());
+
+        return noticeList;
+    }*/
+
+    // 9. 공지사항 게시판의 목록에 대한 요청을 처리하는 API를 만들어 보자.
+    @GetMapping("/api/notice")
+    public List<NoticeModel> notice() {
+
+        List<NoticeModel> noticeList = new ArrayList<>();
+        return noticeList;
     }
 }
