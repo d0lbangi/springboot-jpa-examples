3272c1460fb1a6efe7147e00b5bb1ebc4de0b4b6
Jeongho Park <d0lbangi13@naver.com>
2023-10-12 17:49:12 +0900

    Add NoticeModel and Updated ApiNoticeController

---
+++ src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
@@ -0,0 +1,30 @@
+package com.example.jpa.notice.controller;
+
+
+import com.example.jpa.notice.model.NoticeModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class ApiNoticeController {
+
+//    @GetMapping("/api/notice")
+//    public String noticeString() {
+//        return "공지사항입니다.";
+//    }
+    @GetMapping("/api/notice")
+    public NoticeModel notice() {
+
+        LocalDateTime regDate = LocalDateTime.of(2021, 2, 8, 0,0);
+
+        NoticeModel notice = new NoticeModel();
+        notice.setId(1);
+        notice.setTitle("공지사항 입니다.");
+        notice.setContents("공지사항 내용입니다.");
+        notice.setRegDate(regDate);
+
+        return notice;
+    }
+}
