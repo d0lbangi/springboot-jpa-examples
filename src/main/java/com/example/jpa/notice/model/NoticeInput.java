acb5da25d9d8dff7b1f39341d57aa439e917d1ea
Jeongho Park <d0lbangi13@naver.com>
2023-10-13 17:12:00 +0900

    Put condition on title, contents about each size with exception error messages

--- src/main/java/com/example/jpa/notice/model/NoticeInput.java
+++ src/main/java/com/example/jpa/notice/model/NoticeInput.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -14,9 +15,11 @@ import java.time.LocalDateTime;
 @Data
 public class NoticeInput {
 
+    @Size(min = 10, max = 100, message = "제목은 10-100자 사이의 값 입니다.")
     @NotBlank(message = "제목은 필수입니다.")
     private String title;
 
+    @Size(min = 50, max = 1000, message = "내용은 50-1000자 사이의 값 입니다.")
     @NotBlank(message = "내용은 필수입니다.")
     private String contents;
 }
