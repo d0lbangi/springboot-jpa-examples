da7202dc7354d4391367a4b0cfe59ca992701845
Jeongho Park <d0lbangi13@naver.com>
2023-10-20 12:46:24 +0900

    사용자 목록과 사용자 수를 함께 내리는 REST API 작성

---
+++ src/main/java/com/example/jpa/user/model/ResponseMessage.java
@@ -0,0 +1,19 @@
+package com.example.jpa.user.model;
+
+import com.example.jpa.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ResponseMessage {
+
+    private long totalUserCount;
+    private List<User> data;
+}
