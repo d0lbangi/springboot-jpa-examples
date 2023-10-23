5265ebf224c7572a4598c914a73df11ba6611bf8
Jeongho Park <d0lbangi13@naver.com>
2023-10-23 13:37:42 +0900

    RestTemplate을 이용한 공공데이터토털의 공공API 연동하여 전국약국목록을 가져오는 API 작성

---
+++ src/main/java/com/example/jpa/extra/ApiExtraController.java
@@ -0,0 +1,82 @@
+package com.example.jpa.extra;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.jpa.board.entity.BoardType;
+import com.example.jpa.board.model.*;
+import com.example.jpa.board.service.BoardService;
+import com.example.jpa.common.model.ResponseResult;
+import com.example.jpa.notice.model.ResponseError;
+import com.example.jpa.user.model.ResponseMessage;
+import com.example.jpa.util.JWTUtils;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.engine.spi.CollectionKey;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RestController
+public class ApiExtraController {
+
+    /**
+     * 86. RestTemplate을 이용한 공공데이터토털의 공공API 연동하여 전국약국목록을 가져오는 API 작성
+     * - 공공데이터포털 주소: https://www.data.go.kr/
+     * - 회원가입후 이용가능
+     * - 전국 약국 정보 조회 서비스 키워드 검색이후 활용신청후 조회가능
+     */
+    @GetMapping("/api/extra/pharmacy")
+    public String pharmacy() {
+
+    String apiKey = "b%2B%2B4yXYfkmTA4JAcOcr3Gsx4qvS4i9hJCloSX4w6Nq0s2YyJ3a9V6OTUuBq%2B%2F5GMBNnLh69XyuFUmYFsXPtH7Q%3D%3D";
+    String url = "https://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?serviceKey=%s&pageNo=1&numOfRows=10";
+
+    String apiResult = "";
+
+    try {
+        URI uri = new URI(String.format(url, apiKey));
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        String result = restTemplate.getForObject(uri, String.class);
+
+        apiResult = result;
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return apiResult;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
